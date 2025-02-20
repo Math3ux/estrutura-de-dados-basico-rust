1e798e5c73c98afdb636c5c70af0865fed20473a
Matheus <matheusleidow@gmail.com>
2025-02-20 19:57:10 -0300

    Exemplo básico de vetor, array e tupla

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    // Exemplo Básico de Array
+    let array: [i32, 3] = [1, 2, 3];
+
+    // Exemplo Básico de Vetor
+    let vec = Vec::new();
+    vec.push(1);
+    vec.push(2);
+    vec.push(3);
+
+    // Exemplo Básico de Tupla
+    let tupla: (i32, f64, bool) = (1, 2.0, true);
 }
