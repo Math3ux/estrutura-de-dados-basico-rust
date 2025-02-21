6c345e43567e93c1d7fa977fbf48a09d864cdf03
Matheus <matheusleidow@gmail.com>
2025-02-21 20:49:51 -0300

    Adicionando exemplos de Hash em Rust

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,14 @@ fn main() {
 
     // Exemplo Básico de Tupla
     let tupla: (i32, f64, bool) = (1, 2.0, true);
+
+    // Exemplo Básico de Mapa
+    let mapa: HashMap<String, i32> = HashMap::new();
+    mapa.insert("key1".to_string(), 1);
+    mapa.insert("key2".to_string(), 2);
+
+    // Exemplo Básico de Set
+    let set: HashSet<i32> = HashSet::new();
+    set.insert(1);
+    set.insert(2);
 }
